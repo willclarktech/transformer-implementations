b01620544d41695c17f3ca2e0986f1b781c2b908
willclarktech <willclarktech@users.noreply.github.com>
2024-02-07 13:03:19 +0100

    WIP: reset

--- transformer_implementations/rwkv.py
+++ transformer_implementations/rwkv.py
@@ -3,10 +3,17 @@ Adapted from How the RWKV language model works
 https://johanwind.github.io/2023/03/23/rwkv_details.html
 NOTE: Not actually a transformer
 """
+from typing import Iterable
+
 import torch
 from torch import Tensor
 from torch.nn import Embedding, LayerNorm, Linear, Module
-from torch.nn.functional import one_hot, relu, sigmoid, softmax
+from torch.nn.functional import cross_entropy, one_hot, relu, sigmoid, softmax
+from torch.optim import Adam, Optimizer
+from torch.utils.data import DataLoader, Dataset
+
+torch.autograd.set_detect_anomaly(True)
+
 
 argmax = torch.argmax
 exp = torch.exp
@@ -45,7 +52,7 @@ class TimeMixing(Module):
         y = self.Wout(rwkv)
 
         # Update state
-        self.last_x = x
+        self.last_x = x.detach()
         self.num = exp(-exp(self.decay)) * self.last_num + exp_bonus_k * v
         self.den = exp(-exp(self.decay)) * self.last_den + exp_bonus_k
 
@@ -74,7 +81,7 @@ class ChannelMixing(Module):
         y = sigmoid(r) * vk
 
         # Update state
-        self.last_x = x
+        self.last_x = x.detach()
 
         return y
 
@@ -96,11 +103,11 @@ class RWKV(Module):
         for i in range(self.n_layers):
             x_ = self.layer_norms[i * 2 + 1](x)
             dx = self.time_mixings[i](x_)
-            x += dx
+            x = x + dx
 
             x_ = self.layer_norms[i * 2 + 2](x)
             dx = self.channel_mixings[i](x_)
-            x += dx
+            x = x + dx
 
         x = self.layer_norms[-1](x)
         x = self.head(x)
@@ -108,6 +115,19 @@ class RWKV(Module):
         probs = softmax(x, dim=-1)
 
         return probs
+
+
+class RandomSequenceDataset(Dataset):
+    def __init__(self, sequence_length: int, num_sequences: int) -> None:
+        self.sequence_length = sequence_length
+        self.num_sequences = num_sequences
+
+    def __len__(self) -> int:
+        return self.num_sequences
+
+    def __getitem__(self, _: int) -> tuple[Tensor, Tensor]:
+        sequence = torch.randint(0, 10, (self.sequence_length,))
+        return sequence[:-1], sequence[1:]
 
 
 def greedy_decode(
@@ -131,14 +151,68 @@ def greedy_decode(
     return ys
 
 
+def run_epoch(
+    model: RWKV,
+    optimizer: Optimizer,
+    data_loader: Iterable[tuple[Tensor, Tensor]],
+    max_value: int,
+) -> float:
+    total_loss: float = 0
+    for input_seq, target_seq in data_loader:
+        optimizer.zero_grad()
+        loss = 0
+        for i in range(len(input_seq) - 1):
+            input_token = input_seq[i]
+            target_token = target_seq[i + 1]
+
+            # NOTE: Pylint does not recognize one_hot as a function for some reason
+            # pylint: disable=not-callable
+            input_tensor = one_hot(
+                input_token.clone().detach(),
+                num_classes=max_value,
+            )
+            # NOTE: Pylint does not recognize one_hot as a function for some reason
+            # pylint: disable=not-callable
+            target_tensor = one_hot(
+                target_token.clone().detach(),
+                num_classes=max_value,
+            )
+
+            logits = model(input_tensor)
+            loss += cross_entropy(logits.view(-1, max_value), target_tensor.view(-1))
+
+        loss /= len(input_seq) - 1
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss
+
+
 N_LAYERS = 24
-N_EMBED = 10
 SIZE = 4
 MAX_VALUE = 10
+SEQUENCE_LENGTH = 10
+N_SEQUENCES = 80
+BATCH_SIZE = 32
+LEARNING_RATE = 1
+N_EPOCHS = 20
 
 
 def run() -> None:
-    model = RWKV(N_LAYERS, N_EMBED, SIZE)
+    model = RWKV(N_LAYERS, MAX_VALUE, SIZE)
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+    dataset = RandomSequenceDataset(SEQUENCE_LENGTH, N_SEQUENCES)
+    data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    for epoch in range(N_EPOCHS):
+        model.train()
+        loss = run_epoch(model, optimizer, data_loader, MAX_VALUE)
+        average_loss = loss / len(data_loader)
+        print(f"Epoch {epoch}: average loss = {average_loss}")
+
+    model.eval()
     source = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     output = greedy_decode(model, source, MAX_VALUE)
     print(f"Source: {source}")
